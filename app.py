addb685c4ec1aac04263d33824322e4e85f474ac
ASHISH KUMAR PRADHAN <ashishkumarpradhan@ASHISHs-MacBook-Air.local>
2024-06-29 17:23:03 +0530

    added multiplicatin feature

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from cal_fun import do_addition,do_subtraction
-
+from multiply import do_multiplication
 def main():
     print("Welcome yo the calculator app")
     print("""
           /nselect the function
           1. Add
-          2. Subtract""")
+          2. Subtract
+          3. mulptiply
+          """)
 
     user_input = input("select the function")
 
@@ -17,6 +19,8 @@ def main():
         result = do_addition(a,b)
     elif user_input == '2':
         result = do_subtraction(a,b)
+    elif user_input == '3':
+        result = do_multiplication(a,b)
 
     print('Result:',result)
 
